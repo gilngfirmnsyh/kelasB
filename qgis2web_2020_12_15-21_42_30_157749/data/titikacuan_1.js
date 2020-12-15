f6e26cd400f9e7e488543d343680df722a947d61
gilngfirmnsyh <76056079+gilngfirmnsyh@users.noreply.github.com>
2020-12-15 21:53:54 +0700

    Add files via upload

---
+++ qgis2web_2020_12_15-21_42_30_157749/data/titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.417004859183237, -7.16105270647732 ] } }
+]
+}
